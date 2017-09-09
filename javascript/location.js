4624155a45dd7e734d786c118fb0c4283fe85af8
hpun5 <henrychpun@gmail.com>
2017-09-09 13:10:47 -0400

    map error handling

--- javascript/location.js
+++ javascript/location.js
@@ -1,8 +1,9 @@
 var x = document.getElementById("map");
 window.onload = getLocation();
+
 function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
+    if (navigator.geolocation)	 {
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
     } else { 
         x.innerHTML = "Geolocation is not supported by this browser.";
     }
@@ -11,7 +12,7 @@ function getLocation() {
 function showPosition(position) {
 	var myLatlng = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
      var myOptions = {
-         zoom: 4,
+         zoom: 12,
          center: myLatlng,
          mapTypeId: google.maps.MapTypeId.ROADMAP
          }
@@ -22,3 +23,22 @@ function showPosition(position) {
       title:"Fast marker"
      });
 }
+
+function showError(error) {
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+            x.innerHTML = "User denied the request for Geolocation."
+            break;
+        case error.POSITION_UNAVAILABLE:
+            x.innerHTML = "Location information is unavailable."
+            break;
+        case error.TIMEOUT:
+            x.innerHTML = "The request to get user location timed out."
+            break;
+        case error.UNKNOWN_ERROR:
+            x.innerHTML = "An unknown error occurred."
+            break;
+    }
+}
+
+
